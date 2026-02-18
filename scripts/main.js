bdc81ebae0e988db77bcc1bb7b5f4fd6059bcb8e
RudolfSchultz <RudolfSchultz1998@web.de>
2026-02-18 15:03:06 +0100

    Add Pokémon stats display to the PokeDex

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 
-
+let pokemon1;
 
 
 
@@ -24,15 +24,28 @@ async function getPokemon(id) {
     // console.log(pokemon.forms[0].url);
     let PokemonImage = pokemon.forms[0].url;
     getPokemonImage(PokemonImage);
+    getPokemonStats(pokemon.stats);
 }
 
 async function getPokemonImage(pokemonUrl) {
     let url = pokemonUrl;
     let response = await fetch(url);
-    let pokemon = await response.json();
-    // console.log(pokemon);
-        let PokemonImage = pokemon.sprites.front_default;
+     pokemon1 = await response.json();
+        // console.log(pokemon1);
+        let PokemonImage = pokemon1.sprites.front_default;
+
+
         // console.log(PokemonImage);
         document.getElementById("Pokemon").src = PokemonImage;
 }
 
+async function getPokemonStats(stats) {
+    let stats1 = stats;
+    // console.log(stats1);
+    document.getElementById("HP").innerText = "HP: " + stats1[0].base_stat;
+    document.getElementById("Attack").innerText = "Attack: " + stats1[1].base_stat;
+    document.getElementById("Defense").innerText = "Defense: " + stats1[2].base_stat;
+    document.getElementById("SpecialAttack").innerText = "Special Attack: " + stats1[3].base_stat;
+    document.getElementById("SpecialDefense").innerText = "Special Defense: " + stats1[4].base_stat;
+    document.getElementById("Speed").innerText = "Speed: " + stats1[5].base_stat;
+}
