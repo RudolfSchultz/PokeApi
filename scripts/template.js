52eea7c4232951759eac98938d6025116c3a76ec
RudolfSchultz <RudolfSchultz1998@web.de>
2026-02-18 20:09:47 +0100

    template update

--- scripts/template.js
+++ scripts/template.js
@@ -1,6 +1,6 @@
 function renderPokemonFrontTemplate(id) {
     return `<div class="pokemon-card">
-            <h2 class="pokemon-name">${myPokedex[id].name}</h2>
+            <div class="pokemon-id">#${id} <h2 class="pokemon-name">${myPokedex[id].name}</h2> </div>
              <img class="pokemon-image" src="${myPokedex[id].image}" alt="${myPokedex[id].name}">
              <p class="pokemon-types">${myPokedex[id].types.join(', ')}</p>
              </div>`}; 
