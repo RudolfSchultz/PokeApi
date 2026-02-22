2702359a23403042197df7f5bea42722c730abd7
RudolfSchultz <RudolfSchultz1998@web.de>
2026-02-22 02:01:18 +0100

    Pokemon Generation Function added

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,13 @@ const selectElement1 = document.getElementById('gen-options');
 
 
 async function init() {
+    await getPokemonGen();
     onPageLoad();
-        getPokemonGen();
-        
 
     for (let i = 1; i <= 25; i++) {
         await getPokemon(i);
     }
-    // console.table(myPokedex);
+    // console.table(myPokedex);        
     renderPokemon();
 }
 
@@ -26,40 +25,43 @@ async function getPokemonGen() {
     Object.keys(data.results).forEach(key => {
         GenList.push(data.results[key].name);
     });
-    // console.log(GenList);
 
 
-    //Dropdown befüllen
-        GenList.forEach((gen, index) => {
-            const option = document.createElement('option');
-            option.value = index + 1;
-            option.textContent = gen;
-            selectElement1.appendChild(option);
-        });
+    fillDropdown();
 };
 
-    selectElement1.addEventListener('change', (event) => {
-  const selectedValue = event.target.value;
-  localStorage.setItem('selectedGen', selectedValue);
+selectElement1.addEventListener('change', (event) => {
+    const selectedValue = event.target.value;
+    localStorage.setItem('selectedGen', selectedValue);
 });
 
+
+function fillDropdown() {
+    GenList.forEach((gen, index) => {
+        const option = document.createElement('option');
+        option.value = index + 1;
+        option.textContent = gen;
+        selectElement1.appendChild(option);
+    })
+};
+
 function onPageLoad() {
-  // Prüfen, ob schon mal was ausgewählt wurde
-  const savedGen = localStorage.getItem('selectedGen');
-  if (savedGen) {
-    // Wenn ja: Dropdown auf diesen Wert setzen und Bilder laden
-    selectElement1.value = savedGen;
-  } else {
-    // Wenn nein: Standardwert (z.B. Gen 1) laden
-    selectElement1.value = 6;
-  }
+    // Prüfen, ob schon mal was ausgewählt wurde
+    const savedGen = localStorage.getItem('selectedGen');
+    if (savedGen) {
+        // Wenn ja: Dropdown auf diesen Wert setzen und Bilder laden
+        selectElement1.value = savedGen;
+    } else {
+        // Wenn nein: Standardwert (z.B. Gen 1) laden
+        selectElement1.value = 1;
+    }
 }
 
 
 async function getPokemon(id) {
     let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
     let response = await fetch(url);
-     pokemon = await response.json();
+    pokemon = await response.json();
 
     Object.defineProperty(myPokedex, id, {
         value: {
@@ -99,3 +101,24 @@ function renderPokemon() {
     listContainer.innerHTML = html;
 }
 
+function viewGeneration() {
+    const selectedGen = selectElement1.selectedOptions[0].textContent;
+    console.log(`Ausgewählte Generation: ${selectedGen}`);
+    // Hier kannst du die Logik hinzufügen, um die Pokémon der ausgewählten Generation anzuzeigen
+
+    getPokemonByGeneration(selectedGen);
+
+    // return pokemon.spites.versions[selectedGen][key].front_default;
+
+}
+
+function getPokemonByGeneration(selectedGen) {
+    Object.keys(pokemon.sprites.versions[`${selectedGen}`])
+        .forEach(key => {
+            if (pokemon.sprites.versions[`${selectedGen}`][`${key}`].front_default) {
+                let keys =+ key;
+                console.log(keys)
+            }
+        });
+
+}
