5dda3436c87549600971525f3252360a2789c62b
RudolfSchultz <RudolfSchultz1998@web.de>
2026-02-23 13:32:46 +0100

    Change how styles Height and Width on html screen

--- scripts/template.js
+++ scripts/template.js
@@ -1,6 +1,6 @@
 function renderPokemonFrontTemplate(id) {
     return `<a class="pokemon-card" href="./details.html?id=${id}">
             <div class="pokemon-id">#${id} <h2 class="pokemon-name">${myPokedex[id].name}</h2> </div>
-             <img class="pokemon-image" src="${myPokedex[id].image}" alt="${myPokedex[id].name}">
+            <div class="pokemon-image" style="background-image: url('${myPokedex[id].image}')"></div>
              <p class="pokemon-types">${myPokedex[id].types.join(', ')}</p>
              </a>`}; 
